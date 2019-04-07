bbac872c1501c674ae20ff47f59c48721d4c68d9
Kirill <work.kirillabramov@gmail.com>
2019-04-07 22:57:20 +0300

    Updates

---
+++ precache-manifest.4bcb28e47abe79aeafdaf6dbe78cd707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "acb661f2b741acc1ac70",
+    "url": "/react-todo-hooks/static/css/main.6ff5b0cd.chunk.css"
+  },
+  {
+    "revision": "acb661f2b741acc1ac70",
+    "url": "/react-todo-hooks/static/js/main.acb661f2.chunk.js"
+  },
+  {
+    "revision": "f26fb2918ab63e775a6e",
+    "url": "/react-todo-hooks/static/js/1.f26fb291.chunk.js"
+  },
+  {
+    "revision": "407c8ba4da9e7f1ecfff",
+    "url": "/react-todo-hooks/static/js/runtime~main.407c8ba4.js"
+  },
+  {
+    "revision": "b34d5ca2dd1c3d2cac3f79f59addbb51",
+    "url": "/react-todo-hooks/index.html"
+  }
+];
